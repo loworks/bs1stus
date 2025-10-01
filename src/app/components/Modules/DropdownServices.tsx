adab7ca55701282b0c95b84e7fbcc8038a4f4d37
loworks <low@loworks.org>
2025-10-01 01:21:55 -0400

    first commit

--- src/app/components/Modules/DropdownServices.tsx
+++ src/app/components/Modules/DropdownServices.tsx
@@ -14,6 +14,7 @@ export default function ServicesDropdown({
     { label: "Crisps", href: "/products/crisps" },
     { label: "Better Than Butter", href: "/products/better-than-butter" },
   ];
+
   return (
     <Dropdown
       className={`${className}`}
